3700cc36ee55dfef411df56fd511b8b3429d3999
Stefan Biereigel <stefan@biereigel.de>
2014-09-12 09:39:26 +0200

    add file handling to health server
    The health server got its file handling. verified and tested on x86.

--- src/health_srv.cpp
+++ src/health_srv.cpp
@@ -1,4 +1,5 @@
 #include <cstdio>
+#include <cstdlib>
 #include <ctime>
 #include <iostream>
 #include <fstream>
@@ -15,8 +16,12 @@
 #define	SENSOR_PREFIX	"/sys/class/hwmon/hwmon"
 #define SENSOR_POSTFIX	"/temp1_input"
 #define NUM_SENSORS	6
+#define SEC_THRESH	20
 
 #define LOG_PREFIX	"HLT:\t"
+#define FILENAME_PREFIX		"data/health-"
+#define FILENAME_POSTFIX	".txt"
+#define FILENAME		(string(FILENAME_PREFIX) + hwaddr + string("-") + itos(file_index) + string(FILENAME_POSTFIX)).c_str()
 
 using namespace std;
 
@@ -44,6 +49,15 @@ int main(int argc, char *argv[]) {
 	int i, interval;
 	signed long temp;
 
+	// file handling
+	int file_index = 0;
+	unsigned long time_start;
+	string hwaddr;
+	ofstream output_file;
+	ifstream eth_file("/sys/class/net/eth0/address");
+
+	system("mkdir -p data");
+
 	if (argc != 2) {
 		usage(argv);
 		return -1;
@@ -55,6 +69,11 @@ int main(int argc, char *argv[]) {
 
 	UDP UDPtx(DEST_IP, DEST_PORT);
 
+	// init file handling
+	eth_file >> hwaddr;
+	time_start = time(NULL);
+	output_file.open(FILENAME);
+	output_file << time(NULL) << endl;
 	cout << LOG_PREFIX "Health server initialized." << endl;
 	while(1) {
 		txbuf = "";
@@ -70,8 +89,23 @@ int main(int argc, char *argv[]) {
 			txbuf += string(",") + ltos(temp);
 		}
 		cout << LOG_PREFIX "Packet sent." << endl;
+		output_file << string(txbuf) << endl;
 		txbuf += string("\n");
 		UDPtx.send(txbuf.c_str());
+
+		/* note: when the time gets set back by much, this won't work correctly.
+		 * either set time forward or don't!
+		 */
+		if (time(NULL) - time_start > SEC_THRESH) {
+			output_file.close();
+			output_file.clear();
+			file_index++;
+			output_file.open(FILENAME);
+			output_file << time(NULL) << endl;
+			time_start = time(NULL);
+		}
+
+		// blocking wait until next measurement
 		sleep(interval);
 	}
 	return 0;
